d303ff45c40d92b4e697a70b5509c5781f6d2b87
einsteinly <lieinsteinxi@gmail.com>
2014-05-14 09:28:04 +0000

    1st edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ VigoriseCipRecord::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root '#index'
+  root 'vigorise-cip-record#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
