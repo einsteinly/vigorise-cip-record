8af28d92bd19ed8fff466c58a8e6ad335c06319e
einsteinly <lieinsteinxi@gmail.com>
2014-05-14 09:18:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VigoriseCipRecord::Application.config.secret_key_base = '2b1ea052d3988d5d48c696b203a0bd7440b370ed3a51f29d4f6be93ba3e0ff6d1898031f571908475b2346e67b20cd5d992a5cb4dc37b82d2492022864e74e65'
